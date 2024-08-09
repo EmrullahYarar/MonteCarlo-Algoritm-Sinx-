66cd4f32814dead9505bf87d5d65946866fe91e9
EmrullahYarar <147341379+EmrullahYarar@users.noreply.github.com>
2024-08-09 15:46:59 +0300

    Add files via upload

--- monte_clean.c
+++ monte_clean.c
@@ -1,50 +1,55 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <time.h>
- #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
- struct x_ekseni{
+#define PI 3.14159265358979323846
+
+struct x_ekseni{
      double x;
- };
+};
 
- struct y_ekseni{
-     double y;
- };
+struct y_ekseni{
+    double y;
+};
 
- int main(){
+int main(){
 
-     int a, ctr;
-     double temp,d;
-     time_t t;
-     srand(time(&t));
+    int a, ctr;
+    double temp,d;
+    time_t t;
+    srand(time(&t));
 
-     struct x_ekseni x[10000];
+    struct x_ekseni x[10000];
 
-     struct y_ekseni y[10000];
+    struct y_ekseni y[10000];
 
-     srand((unsigned int)time(NULL));
+    srand((unsigned int)time(NULL));
 
-     // random x values
-     for(ctr=0; ctr<10000; ctr++){
-         x[ctr].x=((double)rand()/(double)(RAND_MAX))*(2);
-     }
-     // random y values
-     for(ctr=0; ctr<10000; ctr++){
-         y[ctr].y=((double)rand()/(double)(RAND_MAX))*(4);
-     }
-     // numbers under the curve
-     a=0;
+    // random x values
+    for(ctr=0; ctr<10000; ctr++){
+        x[ctr].x=((double)rand()/(double)(RAND_MAX))*(PI);
+    }
+    // random y values
+    for(ctr=0; ctr<10000; ctr++){
+        y[ctr].y=((double)rand()/(double)(RAND_MAX))*(PI);
+    }
 
-     for(ctr=0; ctr<10000; ctr++) {
-         if( y[ctr].y < ((x[ctr].x)*x[ctr].x)) {
-             a++;
-         }
-     }
-     printf("\n\tNumbers under the curve: %d", a);
-     // area estimate
-     d = (8)*a/10000;
-     printf("\n\tThe estimate is: %f\n", d);
+    // numbers under the curve
+    a=0;
 
-     return 0;
+    for(ctr=0; ctr<10000; ctr++) {
+        if( y[ctr].y < sin(x[ctr].x)) {
+            a++;
+        }
+    }
 
- }
+    printf("\n\tNumbers under the curve: %d", a);
+
+    // area estimate
+    d = (PI*PI)*a/10000;
+    printf("\n\tThe estimate is: %f\n", d);
+
+    return 0;
+
+}
